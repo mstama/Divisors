28aae9528d911980feb0416aceb926c66ecc7ed2
Marcos <marcostamashiro@hotmail.com>
2017-07-11 11:03:43 -0300

    Initial commit

---
+++ Divisors/Program.cs
@@ -0,0 +1,23 @@
+﻿using Divisors.Services;
+using System;
+
+namespace Divisors
+{
+    internal static class Program
+    {
+        private static readonly char[] _separators = { ' ' };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input numbers:");
+            var input = Console.ReadLine();
+            var values = input.Split(_separators, StringSplitOptions.RemoveEmptyEntries);
+            var numberA = long.Parse(values[0]);
+            var numberB = long.Parse(values[1]);
+            Console.WriteLine("Calculating for {0} and {1}...", numberA, numberB);
+            var brute = new Brute();
+            var output = brute.Calculate(numberA, numberB);
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Divisors/Services/Brute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Divisors.Services
+{
+    public class Brute
+    {
+        public IList<long> Calculate(long a, long b)
+        {
+            var output = new List<long>();
+            output.Add(1);
+            for(long i = 2; i <= a && i <= b; i++)
+            {
+                if(a%i==0 && b % i == 0)
+                {
+                    Console.WriteLine("{0}", i);
+                    output.Add(i);
+                }
+            }
+            return output;
+        }
+    }
+}
