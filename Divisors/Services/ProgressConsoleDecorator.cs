7bc700b0249f92cfed73af9fd7406fc326ad2bcd
Marcos <marcostamashiro@hotmail.com>
2017-07-26 12:54:34 -0300

    License header

--- Divisors/Services/ProgressConsoleDecorator.cs
+++ Divisors/Services/ProgressConsoleDecorator.cs
@@ -1,4 +1,7 @@
-﻿using Divisors.Interfaces;
+﻿// Copyright (c) 2017 Marcos Tamashiro. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Divisors.Interfaces;
 using System;
 using System.Collections.Generic;
 
--- Divisors/Services/TimeoutDecorator.cs
+++ Divisors/Services/TimeoutDecorator.cs
@@ -1,4 +1,7 @@
-﻿using Divisors.Exceptions;
+﻿// Copyright (c) 2017 Marcos Tamashiro. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Divisors.Exceptions;
 using Divisors.Interfaces;
 using System;
 using System.Collections.Generic;
